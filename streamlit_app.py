ec0922629a4ec89e6092f6fbfc4529f46a493e07
VALORIX DATA MANAGEMENT <159240570+VALORIXDM@users.noreply.github.com>
2025-12-02 20:42:26 +0100

    Add requests import and fetch smoothie data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -15,14 +16,13 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
-
-
-
 ingredients_list = st.multiselect('Choose up to 5 ingredients: ', my_dataframe, max_selections=5 )
 
 if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
         ingredients_string += fruit_chosen + ' '
         
     # st.write(ingredients_string)
@@ -36,9 +36,4 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, '+ name_on_order + '!', icon="✅")
-
-# New section to display smoothiefroot nutrition information
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        
